45290fc1e694ea4f87f768a6739fc21ec0334893
Tony O'Connor <toconnor13@gmail.com>
2014-03-20 13:26:16 +0000

    Error when checking for sile size corrected

--- hoover/hoover.py
+++ hoover/hoover.py
@@ -1,16 +1,15 @@
 from twisted.internet import protocol, reactor
 import os
 
-f = open('attendance.csv', 'a')
 class Echo(protocol.Protocol):
 	def dataReceived(self, data):
+		f = open('attendance.csv', 'a')
 		f.write(data)
 		print "Some data received"
-		if os.stat('attendance.csv') > 2000:
+		if os.stat('attendance.csv').st_size > 40000:
 			os.system("mysqlimport --ignore-lines=0 --fields-terminated-by=, --columns='timestamp,rssi,id,sensor_id' --local -u root -p'the power to do what is right' cattalax ./attendance.csv")
 			print "File rolled over."
 			os.remove('attendance.csv')
-			f = open('attendance.csv', 'a')
 
 class EchoFactory(protocol.Factory):
 	def buildProtocol(self, addr):
