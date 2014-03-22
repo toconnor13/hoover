56e56caed4bb709c4e774ea31f49777b8d37b721
Tony O'Connor <toconnor13@gmail.com>
2014-03-22 18:43:42 +0000

    Hour and Day objects updated

--- hoover/time_units/day_creation.py
+++ hoover/time_units/day_creation.py
@@ -1,8 +1,6 @@
 import os, sys
 
-script, day, month, year = sys.argv
-
-path='/home/sheefrex/code/cattalax/site'
+path='/root/.virtualenvs/cattalax/cattalax'
 if path not in sys.path:
 	sys.path.append(path)
 
@@ -17,45 +15,35 @@ setup_environ(settings)
 list_of_shops = [v for v in Outlet.objects.all()]
 
 for shop in list_of_shops:
-	d = Day(vendor=shop, day=day, month=month, year=year, no_of_walkbys=0, no_of_entries=0, no_of_bounces=0, avg_duration=0)
-	
-	
-	list_of_visits = [visit for visit in shop.visit_set.all()]
-	d.no_of_entries = len(list_of_visits)
+	days_to_update = shop.day_set.filter(datetime__gte=timezone.now()-datetime.timedelta(days=7))	
+	for day in days_to_update:
+		list_of_visits = [visit for visit in day.visit_set.all()]
+		day.no_of_entries = len(list_of_visits)
 
-	list_of_walkbys = [walkby for walkby in shop.walkby_set.all()]
-	d.no_of_walkbys = len(list_of_walkbys)
+		list_of_walkbys = [walkby for walkby in day.walkby_set.all()]
+		day.no_of_walkbys = len(list_of_walkbys)
 
-	list_of_bounces = [v for v in list_of_visits if v.duration<60]
-	d.no_of_bounces = len(list_of_bounces)
+		list_of_bounces = [v for v in list_of_visits if v.duration<60]
+		day.no_of_bounces = len(list_of_bounces)
 
-	if d.no_of_entries>0:
-		d.avg_duration = sum([int(v.duration) for v in list_of_visits])/d.no_of_entries
+		if d.no_of_entries>0:
+			day.avg_duration = sum([int(v.duration) for v in list_of_visits])/hour.no_of_entries
 
-	print "Day object created"
+		d.save()
 
-	d.save()
+		for hour in day.hour_set.all():
+			list_of_visits = [visit for visit in hour.visit_set.all()]
+			hour.no_of_entries = len(list_of_visits)
 
+			list_of_walkbys = [walkby for walkby in hour.walkby_set.all()]
+			hour.no_of_walkbys = len(list_of_walkbys)
 
-	for j in range(24):
-		h = Hour(vendor=shop, day=d, hour=j, no_of_walkbys=0, no_of_entries=0, no_of_bounces=0, avg_duration=0)
-		visits_this_hour = [v for v in list_of_visits if v.get_hour()==j]
-		h.no_of_entries = len(visits_this_hour)
-#		print len(visits_this_hour)
-#		print h.no_of_entries
+			list_of_bounces = [v for v in list_of_visits if v.duration<60]
+			hour.no_of_bounces = len(list_of_bounces)
 
-		if h.no_of_entries>0:
-			h.avg_duration = sum([int(v.duration) for v in visits_this_hour])/h.no_of_entries
-		
-		h.no_of_walkbys = len([w for w in list_of_walkbys if w.get_hour()==j])
-		h.no_of_bounces = len([b for b in list_of_bounces if b.get_hour==j])
-		print "Hour object created for " + str(h)
-		h.save()
+			if hour.no_of_entries>0:
+				hour.avg_duration = sum([int(v.duration) for v in list_of_visits])/hour.no_of_entries
 
-
-
-
-
-
+			hour.save()
 
 
