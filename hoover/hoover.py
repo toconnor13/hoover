93605eb96d25c7c82bfca582badbf34b8869e00e
Tony O'Connor <toconnor13@gmail.com>
2014-03-31 10:59:14 +0100

    Error fixed

--- hoover/hoover.py
+++ hoover/hoover.py
@@ -5,12 +5,10 @@ class Echo(protocol.Protocol):
 	def dataReceived(self, data):
 		f = open('attendance.csv', 'a')
 		f.write(data)
-		print "Some data received"
 		os.system("echo Data received `date \"+%d-%m %I-%M\"` >> datalog.txt")
 		if os.stat('attendance.csv').st_size > 40000:
-			os.system("echo `date \"+%d-%m %I-%M\" >> datalog.txt")
+			os.system("echo File rolled over `date \"+%d-%m %I-%M\"` >> datalog.txt")
 			os.system("mysqlimport --ignore-lines=0 --fields-terminated-by=, --columns='timestamp,rssi,id,sensor_id' --local -u root -p'the power to do what is right' cattalax ./attendance.csv >> datalog.txt")
-			print "File rolled over."
 			os.remove('attendance.csv')
 
 class EchoFactory(protocol.Factory):
