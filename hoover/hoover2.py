fd427627957898f1cc7cbaef2fe4e6f9860ec890
Tony O'Connor <toconnor13@gmail.com>
2014-05-08 17:23:51 +0100

    Hoover improved - does not delete file unless write to db is successful

---
+++ hoover/hoover2.py
@@ -0,0 +1,23 @@
+from twisted.internet import protocol, reactor
+import os
+import commands
+
+class Echo(protocol.Protocol):
+	def dataReceived(self, data):
+		f = open('attendance.csv', 'a')
+		f.write(data)
+		os.system("echo Data received `date \"+%d-%m %I-%M\"` >> datalog.txt")
+		if os.stat('attendance.csv').st_size > 40000:
+			os.system("echo File rolled over `date \"+%d-%m %I-%M\"` >> datalog.txt")
+			output = commands.getoutput("mysqlimport --ignore-lines=0 --fields-terminated-by=, --columns='timestamp,rssi,id,sensor_id' --local -u root -p'the power to do what is right' cattalax ./attendance.csv >> datalog.txt")
+			if 'cattalax.attendance: Records' in output:
+				os.remove('attendance.csv')
+				os.system("echo "+output+" >> datalog.txt")
+
+class EchoFactory(protocol.Factory):
+	def buildProtocol(self, addr):
+		return Echo()
+
+
+reactor.listenTCP(4481, EchoFactory())
+reactor.run()
